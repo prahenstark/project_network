21009e0738eb64319589ee7d42994982f3bd9fe6
Arghya Mondal <arghyamondal.work@gmail.com>
2024-10-31 12:21:05 +0000

    todo added

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -6,6 +6,7 @@ export default function DashboardLayout({children}) {
     <main className="flex h-svh bg-muted">
       <Sidebar />
       <div className="content flex-1 overflow-y-scroll">
+        //TODO: put the navbar here
         {children}
       </div>
     </main>
