77fe0e9251c24e60b17b60311c5899dee4411e87
Arghya Mondal <arghyamondal.work@gmail.com>
2024-11-01 05:19:30 +0000

    devices page stats section built

--- src/app/(dashboard)/devices/page.js
+++ src/app/(dashboard)/devices/page.js
@@ -10,8 +10,55 @@ export default function Devices({}) {
       <div className="flex h-full flex-1">
         <ProjectList />
         <div className="flex-1 min-h-svh">
-          <div className="flex">
+          <div className="flex items-center p-6">
 
+            <div className="flex-1 flex items-center gap-2 text-primary">
+              <div className="text-5xl font-bold">02</div>
+              <div className="text-lg font-medium">All</div>
+            </div>
+
+            <div className="flex-1 flex items-center gap-2">
+              <div className="text-5xl font-bold">01</div>
+              <div className="text-lg font-medium">AP</div>
+            </div>
+
+            <div className="flex-1 flex items-center gap-2">
+              <div className="text-5xl font-bold">01</div>
+              <div className="text-lg font-medium">CPE</div>
+            </div>
+
+            <div className="flex-1 flex items-center gap-2">
+              <div className="text-5xl font-bold">01</div>
+              <div className="text-lg font-medium">4G</div>
+            </div>
+
+            <div className="flex-1 flex items-center gap-2">
+              <div className="text-5xl font-bold">01</div>
+              <div className="text-lg font-medium">5G</div>
+            </div>
+
+            <div className="space-y-4 flex-1">
+              <div className="flex gap-4">
+                <div className="flex-1 flex items-center gap-2">
+                  <div className="size-4 rounded-md bg-blue-600" />
+                  <span className="text-xs">Devices: 2</span>
+                </div>
+                <div className="flex-1 flex items-center gap-2">
+                  <div className="size-4 rounded-md bg-green-600" />
+                  <span className="text-xs">Offline: 0</span>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <div className="flex-1 flex items-center gap-2">
+                  <div className="size-4 rounded-md bg-orange-600" />
+                  <span className="text-xs">Alarm: 2</span>
+                </div>
+                <div className="flex-1 flex items-center gap-2">
+                  <div className="size-4 rounded-md bg-white" />
+                  <span className="text-xs">Online: 2</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
